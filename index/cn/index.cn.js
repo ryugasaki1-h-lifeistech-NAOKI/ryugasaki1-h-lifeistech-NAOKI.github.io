bf4ba03366ae4d9249e3b5430a8953c94ff06a8a
ryugasaki1-h-lifeistech-NAOKI <132548994+ryugasaki1-h-lifeistech-NAOKI@users.noreply.github.com>
2023-07-11 14:42:08 +0900

    Add files via upload

---
+++ index/cn/index.cn.js
@@ -0,0 +1,74 @@
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {
+	easing: 'easeInOut',
+	duration: 2000,
+	strokeWidth: 10.5,
+	color: '#f1c21e',
+	trailWidth: 10.5,
+	trailColor: '#ffe176',
+	text: {			
+		style: {
+			position: 'absolute',
+			left: '50%',
+			top: '50%',
+			padding: '0',
+			margin: '-40px 0 0 0',
+			transform:'translate(-50%,-50%)',
+			'font-size':'30px',
+			color: '#f1c21e',
+            fontFamily:'MS Mincho',
+            fontWeight:'bold'
+		},
+		autoStyleContainer: false
+	},
+	step: function(state, bar) {
+		bar.setText('Loading･･･  ' + Math.round(bar.value() * 100) + ' %'); 
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {
+	$("#splash_text").fadeOut(10);
+	$(".loader_cover-up").addClass("coveranime");
+	$(".loader_cover-down").addClass("coveranime");
+	$("#splash").fadeOut();
+});
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から100pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+	var scroll = $(window).scrollTop(); //スクロール値を取得
+	if(scroll > 0){
+		$(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
+            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
+        });	
+	}
+    return false;//リンク自体の無効化
+});
