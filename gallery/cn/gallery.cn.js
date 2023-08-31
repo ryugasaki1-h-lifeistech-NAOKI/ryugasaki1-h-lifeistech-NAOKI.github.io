9ad23361ad72b05f6efb20b3d5574988418a9f6c
ryugasaki1-h-lifeistech-NAOKI <132548994+ryugasaki1-h-lifeistech-NAOKI@users.noreply.github.com>
2023-08-31 18:56:55 +1000

    Add files via upload

--- gallery/cn/gallery.cn.js
+++ gallery/cn/gallery.cn.js
@@ -76,3 +76,74 @@ share_pocket.setAttribute(
 	"href",
 	"http://getpocket.com/edit?url=" + share_url
 );
+
+
+
+//上部画像の設定
+$('.gallery').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	fade: true, //フェードの有効化
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	asNavFor: '.choice-btn', //連動させるサムネイルのクラス名
+});
+
+//選択画像の設定
+$('.choice-btn').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 4, //表示させるスライドの数
+	focusOnSelect: true, //フォーカスの有効化
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	asNavFor: '.gallery', //連動させるスライドショーのクラス名
+});
+
+
+
+
+
+
+$(window).on('load',function(){	//画面遷移時にギャラリーの画像が被らないように、すべての読み込みが終わった後に実行する
+
+	//＝＝＝Muuriギャラリープラグイン設定
+	var grid = new Muuri('.grid', {
+	
+	//アイテムの表示速度※オプション。入れなくても動作します
+	showDuration: 600,
+	showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+	hideDuration: 600,
+	hideEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+		
+	// アイテムの表示/非表示状態のスタイル※オプション。入れなくても動作します
+	  visibleStyles: {
+		opacity: '1',
+		transform: 'scale(1)'
+	  },
+	  hiddenStyles: {
+		opacity: '0',
+		transform: 'scale(0.5)'
+	  }    
+	});
+	
+	//＝＝＝並び替えボタン設定
+	$('.sort-btn li').on('click',function(){			//並び替えボタンをクリックしたら
+		$(".sort-btn .active").removeClass("active");	//並び替えボタンに付与されているactiveクラスを全て取り除き
+		var className = $(this).attr("class");			//クラス名を取得
+		className = className.split(' ');				//「sortXX active」のクラス名を分割して配列にする
+		$("."+className[0]).addClass("active");			//並び替えボタンに付与されているクラス名とギャラリー内のリストのクラス名が同じボタンにactiveクラスを付与
+		if(className[0] == "sort00"){						//クラス名がsort00（全て）のボタンの場合は、
+			 grid.show('');								//全ての要素を出す
+		}else{											//それ以外の場合は
+			grid.filter("."+className[0]); 				//フィルターを実行
+		}
+	});
+	
+	//＝＝＝ Fancyboxの設定
+	$('[data-fancybox]').fancybox({
+	 thumbs: {
+		autoStart: true, //グループのサムネイル一覧をデフォルトで出す。不必要であればfalseに
+	  },	
+	});
+		
+	});
